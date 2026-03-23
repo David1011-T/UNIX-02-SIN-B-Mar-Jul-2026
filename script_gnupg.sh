a31c59bc0620fc65602540374fe9030f872a107a
David1011-T <datorresve@uide.edu.ec>
2026-03-23 12:29:35 +0000

    List secret keys

---
+++ script_gnupg.sh
@@ -0,0 +1,8 @@
+#Listar llaves privadas
+gpg --list-secret-keys --keyid-format=long
+#/home/codespace/.gnupg/pubring.kbx
+#----------------------------------
+#sec   rsa4096/1C9B516DB919F577 2026-03-18 [SC] [expires: 2027-03-18]
+#      A5B18D0E6BF68025BB6F1C431C9B516DB919F577
+#uid                 [ultimate] David Alejandro Torres Vengas (12345678) <davidsitotorres8@gmail.com>
+#ssb   rsa4096/D87B8615073BC8E3 2026-03-18 [E] [expires: 2027-03-18]
