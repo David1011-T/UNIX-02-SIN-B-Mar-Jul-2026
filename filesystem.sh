ef9570aa285f45e677dc4228d0591acd58430acc
David1011-T <datorresve@uide.edu.ec>
2026-03-25 13:43:34 +0000

    test.txt

--- filesystem.sh
+++ filesystem.sh
@@ -2,3 +2,5 @@ pwd #print working directory
 cd #change directory
 ls -F #show files and folders with @
 ls -i #Shows id´s of files
+echo "hola" > test.txt #Create file with text
+stat test.txt #shows information about file
