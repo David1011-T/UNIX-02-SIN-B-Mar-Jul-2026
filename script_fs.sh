36dc651605b0c67f59b075810d6cfbb748d5a411
David1011-T <datorresve@uide.edu.ec>
2026-03-30 13:04:04 +0000

    info_about_disk_command

--- script_fs.sh
+++ script_fs.sh
@@ -1,3 +1,4 @@
-sudo apt update #This command gets all the updates for the system
-sudo apt upgrade #This command upgrades the system with the update from before
-sudo apt install parted #This command installs the app "parted"
+sudo apt update #This command gets all the updates of the packet list
+sudo apt upgrade #This command installs all new versions based on the packet list of the system
+sudo apt install parted #This command downloads and installs the app "parted"
+sudo parted -l && echo -e "\n--\n" && lsblk -f && echo -e #This command gives info about the disk like partitions, blocks, etc...
