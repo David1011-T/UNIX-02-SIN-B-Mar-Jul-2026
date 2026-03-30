7daf25337915232732ad3ba774b149a2818b74df
David1011-T <datorresve@uide.edu.ec>
2026-03-30 12:52:31 +0000

    commands_instalation_parted

---
+++ script_fs.sh
@@ -0,0 +1,3 @@
+sudo apt update #This command gets all the updates for the system
+sudo apt upgrade #This command upgrades the system with the update from before
+sudo apt install parted #This command installs the app "parted"
