ceed0e4f476d53f33bca020c15b0a98c06a2e9a3
David1011-T <datorresve@uide.edu.ec>
2026-03-30 13:48:41 +0000

    file_info_bytes_blocks

--- script_fs.sh
+++ script_fs.sh
@@ -3,3 +3,5 @@ sudo apt upgrade #This command installs all new versions based on the packet lis
 sudo apt install parted #This command downloads and installs the app "parted"
 sudo parted -l && echo -e "\n--\n" && lsblk -f && echo -e #This command gives info about the disk like partitions, blocks, loops.
 [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #This command makes a conditional test to verify that the file /sys/firmware/efi exists and if so the test is true.
+echo "Esto es un archivo" > archivo.txt #This command creates a file with text: "Esto es un archivo"
+stat archivo.txt #This command shows info about the file
