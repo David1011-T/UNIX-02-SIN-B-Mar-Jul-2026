71f6396724aa7817b00cccbe7ad29652020c997a
David1011-T <datorresve@uide.edu.ec>
2026-04-06 12:37:50 +0000

    route_proyecto

--- script_boot.sh
+++ script_boot.sh
@@ -6,3 +6,9 @@ cd /home/codespace
 cd ~
 #Changes the directory to home
 cd $HOME 
+#Creates a route named "proyecto"
+mkdir proyecto
+#Changes the directory to the new route "proyecto"
+cd proyecto/
+#Shows info about the file were on like the inode, characteristics and hidden files
+ls -lai
