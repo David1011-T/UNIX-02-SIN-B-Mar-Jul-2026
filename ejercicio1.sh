26254732758bfc4881d0f23b006c02f8fd2816bb
David1011-T <datorresve@uide.edu.ec>
2026-04-15 13:09:22 +0000

    chmod hola.sh

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -1,7 +1,8 @@
 echo '#!/bin/sh' > hola.sh #Creates a file hola.sh and writes in it "#!/bin/sh" 
-#Adds "Hola desde mi primer script" in the file hola.sh
-echo 'echo "Hola desde mi primer script" >> hola.sh 
-#Calls the file hola.sh
-cat hola.sh 
-#Executes the file hola.sh
-./hola.sh
+echo 'echo "Hola desde mi primer script"' >> hola.sh #Adds "Hola desde mi primer script" in the file hola.sh
+cat hola.sh #Calls the file hola.sh
+./hola.sh #Executes the file hola.sh
+ls -l hola.sh #Lists the file hola.sh
+chmod +x hola.sh #Changes permission to the file hola.sh
+ls -l hola.sh #Lists the file hola.sh
+./hola.sh #Executes the file hola.sh
