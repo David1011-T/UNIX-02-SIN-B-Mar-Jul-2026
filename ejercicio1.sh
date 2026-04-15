a3ee2330fa2c1389cc5280deb27ae1fc7707aed9
David1011-T <datorresve@uide.edu.ec>
2026-04-15 13:23:13 +0000

    touch /etc/prueba.txt and apt install cowsay needs sudo because they are changing the operating system directly

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -6,3 +6,7 @@ ls -l hola.sh #Lists the file hola.sh
 chmod +x hola.sh #Changes permission to the file hola.sh
 ls -l hola.sh #Lists the file hola.sh
 ./hola.sh #Executes the file hola.sh
+ls /etc #Lists the file /etc
+sudo touch /etc/prueba.txt #Creates a file "Prueba.txt" in the location /etc
+mkdir ~/mi_carpeta #Creates a folder "mi_carpeta" in the home directory
+sudo apt install cowsay #Installs the app cowsay
