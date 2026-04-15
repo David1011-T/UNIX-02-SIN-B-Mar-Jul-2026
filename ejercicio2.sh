f94d9f8ef679a6b9a3f5dfdc0efd136f61f43df8
David1011-T <datorresve@uide.edu.ec>
2026-04-15 13:37:49 +0000

    chmod numbers

---
+++ ejercicio2.sh
@@ -0,0 +1,5 @@
+touch prueba.txt #Creates empty file "prueba.txt"
+chmod 600 prueba.txt #Changes permission to read and write to user for the file prueba.txt
+ls -l prueba.txt #Lists the file prueba.txt
+chmod 755 prueba.txt #Changes the permission to execute and write to user for the file prueba.txt
+ls -l prueba.txt #Lists the file prueba.txt
