1fc7f5b13bf2a46648bff9552eba1ea021893b0e
David1011-T <datorresve@uide.edu.ec>
2026-05-04 13:28:22 +0000

    groups

--- security.sh
+++ security.sh
@@ -1,2 +1,5 @@
 id #This command gives the id for the user, the primary group and the others groups
 cat /etc/passwd | head -10 #This command calls what´s inside the folder /etc/passwd and gives only the first 10 lines
+cat /etc/group | head -10 #This command calls what´s inside the folder /etc/group and gives only the first 10 lines
+groups # Shows the groups 
+groups $USER #Shows the groups the actual user is in
