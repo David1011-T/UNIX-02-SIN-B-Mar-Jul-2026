5b75ad7b02aa4084a5680037870828fd06334847
David1011-T <datorresve@uide.edu.ec>
2026-05-04 12:46:12 +0000

    id-cat-head

---
+++ security.sh
@@ -0,0 +1,2 @@
+id #This command gives the id for the user, the primary group and the others groups
+cat /etc/passwd | head -10 #This command calls what´s inside the folder /etc/passwd and gives only the first 10 lines
