32b55f5ec8095a31b1690c5de356eb86de9e3079
David1011-T <datorresve@uide.edu.ec>
2026-05-06 13:32:02 +0000

    practice_scene

--- security.sh
+++ security.sh
@@ -59,3 +59,10 @@ id root #Lost all the other groups
 #Restore
 sudo usermod -aG diseno,marketing,grupo_temporal root
 id root #Restored
+
+#Prepare practice scene
+mkdir -p ~/lab_chgrp/{proyectos,reportes,scripts}
+touch ~/lab_chgrp/proyectos/app.py
+touch ~/lab_chgrp/proyectos/config.json
+touch ~/lab_chgrp/reportes/informe.txt
+touch ~/lab_chgrp/scripts/deploy.sh
