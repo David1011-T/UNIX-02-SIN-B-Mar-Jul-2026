88162257c46d7a7f1c02b04e1ece7cf72b0ec6b8
David1011-T <datorresve@uide.edu.ec>
2026-05-06 13:33:46 +0000

    ls -la ~/lab_chgrp/

--- security.sh
+++ security.sh
@@ -66,3 +66,6 @@ touch ~/lab_chgrp/proyectos/app.py
 touch ~/lab_chgrp/proyectos/config.json
 touch ~/lab_chgrp/reportes/informe.txt
 touch ~/lab_chgrp/scripts/deploy.sh
+#View initial stat - everyone has the user group
+ls -la ~/lab_chgrp/proyectos/
+ls -la ~/lab_chgrp/reportes/
