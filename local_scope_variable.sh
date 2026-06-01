24c9d544a0f02f250c9eedfefe7dde69fe30f88a
David1011-T <datorresve@uide.edu.ec>
2026-06-01 13:52:54 +0000

    Global_and_local_variable

--- local_scope_variable.sh
+++ local_scope_variable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PUBLISHER="No Starch Press"
+PUBLISHER="No Starch Press" #PUBLISHER is a global variable, a global variable can be used anywhere outside the script
 print_name(){
- local name
+ local name #Name is a local variable, a local variable can only be used inside the script
  name="Black Hat Bash"
  echo "${name} by ${PUBLISHER}"
 }
-print_name
+print_name #To call the function is only needed to put the variable's name
 echo "Variable ${name} will not be printed because it is a local variable."
