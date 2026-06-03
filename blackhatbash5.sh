034dd8d62d631f2559cd292d14a8a9ee2cf37df4
David1011-T <datorresve@uide.edu.ec>
2026-06-03 13:32:41 +0000

    Redirection Operators

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,17 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "El comando lzl falló"
+echo "uno" > archivo.txt
+cat archivo.txt
+echo "uno-uno" > archivo.txt
+cat archivo.txt
+echo "dos-dos" >> archivo.txt
+cat archivo.txt
+echo "tres" &> archivo.txt
+cat archivo.txt
+echo "tres" &>> archivo.txt
+cat archivo.txt
+ls -l / &> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+lzl 2> error.txt
+cat error.txt
