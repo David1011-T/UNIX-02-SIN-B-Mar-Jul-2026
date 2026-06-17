9ce4437366502d9f83cddcdafd9c77f05ee3aa45
David1011-T <datorresve@uide.edu.ec>
2026-06-17 13:17:31 +0000

    fg_bg_ctrl-z

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -40,3 +40,24 @@ cat newlog57.txt
 sed -n '2,15 p' log.txt 
 
 sed -i '1d' log.txt
+
+sleep 100 &
+#[1] 23748
+
+ps -ef | grep sleep
+#root           1       0  0 12:11 ?        00:00:00 /bin/sh -c echo Container started trap "exit 0" 15  exec "$@" while sleep 1 & wait $!; do :; done -
+#root       23748   14217  0 13:10 pts/0    00:00:00 sleep 100
+#root       23974       1  0 13:10 ?        00:00:00 sleep 1
+#root       23977   14217  0 13:10 pts/0    00:00:00 grep --color=auto sleep
+
+jobs
+#[1]+  Hecho                      sleep 100
+
+fg %1
+#sleep 100
+
+CTRL-Z sleep 100
+#[1]+  Detenido                   sleep 100
+
+bg %1
+#[1]+ sleep 100 &
